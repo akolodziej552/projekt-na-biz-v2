56002d613584e5fb2a25837eb3efb53532ed2aad
Aleksander Kołodziej <ciastek.gamer.320elo@gmail.com>
2026-01-22 22:24:00 +0100

    chce spac >:(

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,23 +7,21 @@ import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
 import Menu from "./pages/Menu";
 import "../styles/app.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
     return (
         <div className="app">
-            <BrowserRouter>
-                <Header /> 
-                <Nav />
-                <Routes>
-                    <Route path="/" element={<Main />} />
-                    <Route path="/auth" element={<Auth />} />
-                    <Route path="/contact" element={<Contact />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/menu" element={<Menu />} />
-                </Routes>
-                <Footer />
-            </BrowserRouter>
+            <Header /> 
+            <Nav />
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/auth" element={<Auth />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/menu" element={<Menu />} />
+            </Routes>
+            <Footer />
         </div>
     )
 };
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,8 +1,11 @@
 import { createRoot } from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import "../styles/ArkuszMeyera.css";
 import "../styles/index.css";
 import App from "./App";
 
 createRoot(document.getElementById("root")).render(
-        <App />
+        <HashRouter>
+                <App />
+        </HashRouter>
 )
