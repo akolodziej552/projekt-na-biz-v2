b431072427be2925766943784a46d39cdf14ef17
Aleksander Kołodziej <ciastek.gamer.320elo@gmail.com>
2026-02-19 21:22:52 +0100

    final2

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/projekt-na-biz-v2/"
+  base: "./",
+  build: {
+    outDir: 'dist'
+  }
 })
